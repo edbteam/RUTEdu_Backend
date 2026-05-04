6ca1b167f706ca985f09c9f50ccc2a5fa57a0c3e
xKamileeq <158305872+xKamileeq@users.noreply.github.com>
2026-05-04 15:38:06 +0200

    Add schema and config options to hello route for improved API documentation and validation

--- src/routes/get/hello.ts
+++ src/routes/get/hello.ts
@@ -1,7 +1,26 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
 
 export default {
 	paths: ["/hello", "/world", "/test*"],
+	schema: {
+		tags: ["get"],
+		summary: "Returns a hello world message",
+		description: "Simple test endpoint used to verify if backend is running.",
+		querystring: z.object(
+			{
+			
+			}
+		),
+		response: {
+			200: z.string().describe("Hello world response")
+		}
+	},
+	config: {
+		swagger: {
+			// exposeHeadRoute: true,
+		}
+	},
 	handler: async (request: FastifyRequest, reply: FastifyReply) => {
 		return "Hello World";
 	}
